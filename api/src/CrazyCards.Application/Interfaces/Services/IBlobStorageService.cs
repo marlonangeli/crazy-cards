1a439b689f27f8570267c49a1386ae2df1bcde48
Marlon Angeli <marlonangeli91@gmail.com>
2023-06-16 00:38:58 -0300

    feat: add BlobStorageService

---
+++ api/src/CrazyCards.Application/Interfaces/Services/IBlobStorageService.cs
@@ -0,0 +1,8 @@
+﻿namespace CrazyCards.Application.Interfaces.Services;
+
+public interface IBlobStorageService
+{
+    Task<string> UploadAsync(string fileName, Stream content, CancellationToken cancellationToken);
+    Task<Stream> DownloadAsync(string fileName, CancellationToken cancellationToken);
+    Task<bool> DeleteAsync(string fileName, CancellationToken cancellationToken);
+}
---
+++ api/src/CrazyCards.Infrastructure/Storage/BlobStorageService.cs
@@ -0,0 +1,33 @@
+﻿using Azure.Storage.Blobs;
+using CrazyCards.Application.Interfaces.Services;
+using CrazyCards.Infrastructure.Settings;
+using Microsoft.Extensions.Configuration;
+
+namespace CrazyCards.Infrastructure.Storage;
+
+public class BlobStorageService : IBlobStorageService
+{
+    private readonly BlobContainerClient _containerClient;
+
+    public BlobStorageService(IConfiguration configuration)
+    {
+        var azureSettings = configuration.GetSection("AzureStorage").Get<AzureStorageSettings>()!;
+        var blobServiceClient = new BlobServiceClient(azureSettings.ConnectionString);
+        _containerClient = blobServiceClient.GetBlobContainerClient(azureSettings.ContainerName);
+    }
+
+    public Task<string> UploadAsync(string fileName, Stream content, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Stream> DownloadAsync(string fileName, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(string fileName, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
